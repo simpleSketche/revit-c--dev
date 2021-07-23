042284f6df95b720eeac03d889e6fb267ef3adc4
simple sketch <71196100+simpleSketche@users.noreply.github.com>
2021-07-23 10:58:56 -0400

    intro02-mathfun
    intro02-mathfun

---
+++ intro_dev/intro02-MathFun/intro02-MathFun/intro02-mathfun.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intro01_MathFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int itemACount = 10000;
+            float itemAPrice = 1.66f;
+            int itemBCount = 9843;
+            float itemBPrice = 2.45f;
+            float theSum = getSum(itemACount, itemAPrice, itemBCount, itemBPrice);
+
+            Console.WriteLine($"The sum between itemA and itemB is: {0}", theSum);
+        }
+        public static float getSum(int itemACount, float itemAPrice, int itemBCount, float itemBPrice)
+        {
+            return itemACount * itemAPrice + itemBCount * itemBPrice;
+        }
+    }
+}
