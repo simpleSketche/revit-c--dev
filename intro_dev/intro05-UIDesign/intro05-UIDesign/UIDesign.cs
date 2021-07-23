72eecc538fa3fd5b9bc5de5af82bf63741215a8b
simple sketch <71196100+simpleSketche@users.noreply.github.com>
2021-07-23 14:59:29 -0400

    intro05 - uidesign
    intro05 - uidesign

---
+++ intro_dev/intro05-UIDesign/intro05-UIDesign/UIDesign.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intro05_UIDesign
+{
+    [Transaction(TransactionMode.Manual)]
+    public class UIDesign : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Welcome login = new Welcome();
+            login.ShowDialog();
+            
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ intro_dev/intro05-UIDesign/intro05-UIDesign/Welcome.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace intro05_UIDesign
+{
+    /// <summary>
+    /// Interaction logic for Welcome.xaml
+    /// </summary>
+    public partial class Welcome : Window
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if(Username.Text == "yangyankun" && Password.Password == "shengri12")
+            {
+                MessageBox.Show("Login in successfully.");
+                DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show("Failed Attempt.");
+                DialogResult = false;
+            }
+        }
+    }
+}
