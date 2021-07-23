5ba4185ff39ee2b3b404d9af2cd676d5562dbc5d
simple sketch <71196100+simpleSketche@users.noreply.github.com>
2021-07-23 19:47:39 -0400

    intro06-filteandselectrinfo
    intro06-filteandselectrinfo

---
+++ intro_dev/intro06-ElementExtraction/FilterInfo/filterinfo.cs
@@ -0,0 +1,45 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterInfo
+{
+    [Transaction(TransactionMode.ReadOnly)]
+    public class filterinfo : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            /*Current running revit UI document*/
+            var doc = commandData.Application.ActiveUIDocument.Document;
+            var collector = new FilteredElementCollector(doc);
+
+            /*option1, faster*/
+/*            collector.OfCategory(BuiltInCategory.OST_Doors).OfClass(typeof(FamilyInstance));*/
+
+
+            /*option2*/
+            var familyInstanceFilter = new ElementClassFilter(typeof(FamilyInstance));
+            var categoryFilter = new ElementCategoryFilter(BuiltInCategory.OST_Doors);
+            var doorInstanceFilter = new LogicalAndFilter(familyInstanceFilter, categoryFilter);
+            collector.WherePasses(doorInstanceFilter);
+
+/*            option1 - select all the elements*/
+              var elementList = new List<Element>();
+            foreach (Element item in collector)
+            {
+                elementList.Add(item);
+            }
+
+            /*option2-select all the elements*/
+            /*var selectedDoors = from elem in collector where elem.Name == "Entrance door" select elem;*/
+
+            TaskDialog.Show("result", $"The number doors in the door family called Entrance door is {elementList.Count}");
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ intro_dev/intro06-ElementExtraction/informationRetriever/informationretriever.cs
@@ -0,0 +1,68 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace informationRetriever
+{
+    [Transaction(TransactionMode.ReadOnly)]
+    public class Inforetriever : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            /*Open the current running revit file*/
+            var unidoc = commandData.Application.ActiveUIDocument;
+            /*Find its document variable*/
+            var doc = unidoc.Document;
+            try
+            {
+
+                /*Option1: Select element by element id*/
+                var wallFilter = new WallSelectionFilter();
+                unidoc.Selection.SetElementIds(new List<ElementId> { new ElementId(157139) });
+
+
+
+                /*Option2: Select element by manually selection in revit*/
+                /*Pick the object by element*//*
+                var reference = unidoc.Selection.PickObject(ObjectType.Element, wallFilter);
+                *//*Get the element by its reference ID*//*
+                var element = doc.GetElement(reference);
+                *//*Select the wall element.*//*
+                var wall = element as Wall;
+                TaskDialog.Show("Wall", element.Name);*/
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+
+            }
+
+            
+            return Result.Succeeded;
+        }
+    }
+    public class WallSelectionFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            if(elem.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Walls)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
