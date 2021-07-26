b4374f658171678dbdd5a71dc229524eaa680f75
simple sketch <71196100+simpleSketche@users.noreply.github.com>
2021-07-25 23:54:35 -0400

    intro08 02
    intro08 02

--- intro_dev/intro08-UCD/CUD_Practice 02/Class1.cs
+++ intro_dev/intro08-UCD/CUD_Practice 02/Class1.cs
@@ -20,12 +20,12 @@ namespace CUD_Practice
 
             // ui document
             var uidoc = commandData.Application.ActiveUIDocument;
-/*            var line = Line.CreateBound(new XYZ(0, 0, 0), new XYZ(10, 10, 0));*/
-           
+            /*            var line = Line.CreateBound(new XYZ(0, 0, 0), new XYZ(10, 10, 0));*/
+
             // Find out the level where the wall will be placed at.
             var levelList = from element in new FilteredElementCollector(doc).OfClass(typeof(Level)) where element.Name == "Level 1" select element;
             var first_level = levelList.FirstOrDefault() as Level;
-            
+
             // find out the door family and door location to be placed at.
             /*var door_Loc = line.Evaluate(0.5, true);*/
             var doorID = new ElementId(59772);
@@ -39,38 +39,29 @@ namespace CUD_Practice
 
             try
             {
-                var numWalls = 30;
-                var r1 = 10;
-                var r2 = 25;
-                var rotation = 2 * Math.PI / numWalls;
+                var line = Line.CreateBound(new XYZ(0, 0, 0), new XYZ(0, 10, 0));
+                var beamID = new ElementId(116116);
+                var beamSymbol = doc.GetElement(beamID) as FamilySymbol;
+                var transCreate = new Transaction(doc, "create a beam");
 
-                var transCreate = new Transaction(doc, "create a wall");
                 transCreate.Start();
-
-                for(int i = 0; i < numWalls; i++)
+                if (!beamSymbol.IsActive)
                 {
-                    var line = Line.CreateBound(new XYZ(r1 * Math.Cos(rotation * i), r1 * Math.Sin(rotation * i), 0),
-                        new XYZ(r2*Math.Cos(rotation*i), r2*Math.Sin(rotation*i), 0));
-                    var door_Loc = line.Evaluate(0.5, true);
-                    var wall = Wall.Create(doc, line, first_level.Id, false);
-                    // add the door instance into the new created wall using the method below.
-                    doc.Create.NewFamilyInstance(door_Loc, doorSymbol, wall, Autodesk.Revit.DB.Structure.StructuralType.NonStructural);
-                    
-                    // refresh revit to show each individual change/transaction made to the running document.
-                    doc.Regenerate();
-                    uidoc.RefreshActiveView();
-                    Thread.Sleep(100);
+                    beamSymbol.Activate();
                 }
-               
+                doc.Create.NewFamilyInstance(line, beamSymbol, first_level, Autodesk.Revit.DB.Structure.StructuralType.Beam);
+
                 transCreate.Commit();
             }
             catch (Exception e)
             {
                 TaskDialog.Show("error", e.Message);
+                return Result.Failed;
             }
-            
+
 
             return Result.Succeeded;
         }
     }
 }
+
